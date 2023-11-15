65dcb44928f316849bb8eea9ea9777822a72efe8
Michael H <84158733+Michael-Schoo@users.noreply.github.com>
2023-11-15 13:07:22 +1100

    fix image

--- components/MDX/Image.tsx
+++ components/MDX/Image.tsx
@@ -1,15 +1,22 @@
+'use client';
+
+import { usePathname } from "next/navigation";
+import { addBasePath } from "next/dist/client/add-base-path";
+
 export function Image({ src, alt }: { src: string, alt: string }) {
     if (src.startsWith('./')) {
         
     }
+    const pathname = usePathname();
+    const newSrc = addBasePath(pathname + src.replace('./', ''));
 
     return (
         <>
             {/* <p className="no-text"> */}
                 <div className="gallery">
                     <figure className="gallery-image">
-                        <a href={src} target="_blank">
-                            <img src={src} alt={alt} className="gallery-image" loading="lazy" />
+                        <a href={newSrc} target="_blank">
+                            <img src={newSrc} alt={alt} className="gallery-image" loading="lazy" />
                         </a>
                         <figcaption>{alt}</figcaption>
                     </figure>
